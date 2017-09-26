d369db0dcb529738e3d42ecea169a8fc67d9ee43
Vascohu <hzhphoenix@gmail.com>
2017-09-26 10:13:00 +0800

    The incentive mechanism is implemented and tested. In this version, we change a little on the label_mat. In the previous version, we have a separate true_label vector to store the true labels. In this version, we put the true labels in the last column of the label_mat. This change requires a further revision on the InferModule.

---
+++ MechModule.py
@@ -0,0 +1,103 @@
+# MechModule receives the parameter from RLModule and decides the payment for agents' actions
+
+import abc
+import numpy as np
+
+
+# The base class of incentive mechanisms
+class MechBase(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def set(self, parameters: list):
+        """Set the mechanism parameters"""
+        """We use a list to represent the parameters to incorporate multiple parameters in future versions"""
+        return
+
+    @abc.abstractmethod
+    def pay(self, label_mat: np.matrix):
+        """Decide the payment for workers """
+        return
+
+
+# DG13 Mechanism
+class DG13(MechBase):
+
+    # The parameter of DG13 mechanism
+    d = 25
+
+    def __init__(self):
+        # The incentive level
+        self.B = 0.0
+        # The label mat
+        self.label_mat = None
+
+    def set(self, parameters: list):
+        self.B = parameters[0]
+
+    def pay(self, label_mat: np.matrix):
+        """Compute the reward vector"""
+        reward_vec = []
+        self.label_mat = label_mat
+        for i in range(label_mat.shape[1] - 1):
+            reward_vec.append(self.reward_a_agent(i) * self.B)
+        return reward_vec
+
+    def reward_a_agent(self, agent_id: int):
+        """Compute the reward for one agent"""
+        reward = 0
+        for i in range(self.label_mat.shape[0]):
+            if self.label_mat[i, agent_id] != 0:
+                ref_agent_id = self.ref_agent(agent_id, i)
+                if self.label_mat[i, agent_id] == self.label_mat[i, ref_agent_id]:
+                    reward += 1
+                reward -= self.non_overlapping_set(i, agent_id, ref_agent_id)
+        return reward
+
+    def ref_agent(self, agent_id: int, task_id: int):
+        """Randomly select the reference agent"""
+        if self.label_mat[task_id, -1] != 0:
+            return self.label_mat.shape[1] - 1
+        else:
+            id_set = []
+            for w in range(self.label_mat.shape[1]):
+                if w != agent_id and self.label_mat[task_id, w] != 0:
+                    id_set.append(w)
+            return np.random.choice(id_set)
+
+    def non_overlapping_set(self, task_id: int, agent_id1: int, agent_id2: int):
+        """Compute the reports on the non-overlapping set"""
+        '''Calculate the non-overlapping task sets'''
+        non_overlapping_set1 = []
+        non_overlapping_set2 = []
+        for i in range(self.label_mat.shape[0]):
+            if i != task_id:
+                if (self.label_mat[i, agent_id1] != 0) and (self.label_mat[i, agent_id2] == 0):
+                    non_overlapping_set1.append(i)
+                elif (self.label_mat[i, agent_id1] == 0) and (self.label_mat[i, agent_id2] != 0):
+                    non_overlapping_set2.append(i)
+        if len(non_overlapping_set1) < self.d or len(non_overlapping_set2) < self.d:
+            print("The inappropriate d!!!")
+            exit()
+        '''Sample d non-overlapping tasks'''
+        set1 = np.random.choice(non_overlapping_set1, self.d, replace=False)
+        set2 = np.random.choice(non_overlapping_set2, self.d, replace=False)
+        '''Compute the penalty item in DG13'''
+        p1 = self.count_a_set(agent_id1, set1)
+        p2 = self.count_a_set(agent_id2, set2)
+        penalty = (p1[0]*p2[0]+p1[1]*p2[1])/self.d/self.d
+        return penalty
+
+    def count_a_set(self, worker_id: int, task_set: list):
+        """Count the number of '1' and '2' for the task set"""
+        c = [0]*2
+        for i in task_set:
+            label = self.label_mat[i, worker_id]
+            c[label-1] += 1
+        return c
+
+
+
+
+
+
--- SimuCtrl.py
+++ SimuCtrl.py
@@ -1,10 +1,12 @@
 import numpy as np
 import RLModule
 import InferModule
+import MechModule
 
-worker_num = 5
+worker_num = 4
 task_num = 100
 
+'''
 for i in range(20):
     alpha = np.zeros(shape=(worker_num, 2, 2))
     for j in range(worker_num):
@@ -28,6 +30,66 @@ for i in range(20):
     gs = InferModule.GibbsSampling(task_num, worker_num, 2)
     acc = gs.test(label_mat, true_label)
     print(acc,"\t",gs.ex_accuracy)
+'''
+
+
+def assign_task(task_num: int, worker_num: int, worker_num_per_task: int):
+    """Assign tasks to workers"""
+    task_assign_table = [ [] for _ in range(worker_num)]
+    worker_id = 0
+    for i in range(task_num):
+        for k in range(worker_num_per_task):
+            # print(worker_id)
+            task_assign_table[worker_id].append(i)
+            worker_id += 1
+            if worker_id >= worker_num:
+                worker_id = 0
+    return task_assign_table
+
+
+label = np.zeros(shape=(task_num, worker_num+1), dtype=int)
+task_assign_table = assign_task(task_num, worker_num, 3)
+true_label = np.random.randint(1, 3, task_num)
+for j in range(worker_num):
+    for i in task_assign_table[j]:
+        label[i, j] = true_label[i]
+for i in range(task_num):
+    label[i, -1] = 0# true_label[i]
+
+'''
+# Lazy Strategy - fully random report
+
+for i in task_assign_table[0]:
+    label[i, 0] = np.random.randint(1, 3)
+'''
+
+'''
+# Lazy Strategy - report '1'
+for i in task_assign_table[0]:
+    label[i, 0] = 1
+'''
+
+'''
+# Lazy Strategy - report '2'
+for i in task_assign_table[0]:
+    label[i, 0] = 2
+'''
+
+'''
+# Adversarial Strategy
+for i in task_assign_table[0]:
+    label[i, 0] = 3 - true_label[i]
+'''
+
+# print(task_assign_table)
+# print(label)
+
+mech = MechModule.DG13()
+mech.set([1.0])
+p = mech.pay(np.asmatrix(label))
+print(p)
+
+
 
 
 
