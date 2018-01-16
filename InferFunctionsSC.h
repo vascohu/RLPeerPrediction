447f5282ccd906375bab3952bbef2303eb288ed1
YitaoLiang <yliang@fandm.edu>
2018-01-16 12:57:36 -0800

    clean up

--- InferFunctionsSC.h
+++
@@ -1,5 +0,0 @@
-void init_class(int _task_num, int _worker_num, double *_yDist, double *_pVec, double *_xVec);
-
-double infer(int *pLabelMat);
-
-void deconstruct();
--- setupSC.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-
-ext = Extension("InferModuleSC", sources=["InferModuleSC.pyx", "InferFunctionsSC.cpp", "SFMT\SFMT.c"], language="c++")
-
-setup(
-  ext_modules = cythonize([ext])
-)
